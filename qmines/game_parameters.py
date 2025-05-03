3afb130ea877c46169576b0c438eb24ef9e90545
Bence Racskó <bracsko94@gmail.com>
2025-05-03 11:35:18 +0200

    Removed density mechanism

--- qmines/game_parameters.py
+++ qmines/game_parameters.py
@@ -7,18 +7,12 @@ class GameParameters:
     MAX_SIZE: Final[int] = 30
     SIZE_RANGE: Final[range] = range(MIN_SIZE, MAX_SIZE + 1)
 
-    def __init__(self, n_rows: int, n_cols: int, *, n_mines: int | None = None, density: float | None = None) -> None:
+    def __init__(self, n_rows: int, n_cols: int, *, n_mines: int) -> None:
         self._verify_length(n_rows)
         self._verify_length(n_cols)
         self._n_rows = n_rows
         self._n_cols = n_cols
 
-        if n_mines is None and density is None:
-            raise ValueError('Either mine number or mine density must be specified.')
-        
-        if density is not None:
-            n_mines = round(self.size * density)
-        n_mines = cast(int, n_mines)
         self._verify_mine_number(n_mines)
         self._n_mines = n_mines
 
@@ -73,4 +67,3 @@ class GameParameters:
         i, j = self.wrap_coordinates(i, j)
         return i * self.n_cols + j
     
-    #TODO: Remove density mechanism
