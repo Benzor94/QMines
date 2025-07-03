9f536bfdc316667c427e01092b644951cc75d736
Bence Racskó <bracsko94@gmail.com>
2025-07-03 12:17:00 +0200

    delete

--- src/qmines/__init__.py
+++ src/qmines/__init__.py
@@ -1,14 +1,5 @@
-import PySide6.QtWidgets as qw
 
-from qmines.game_parameters.settings_reader import read_settings
-from qmines.main_window import MainWindow
-from qmines.state_processor import StateProcessor
 
 def main() -> None:
     
-    app = qw.QApplication()
-    parameters = read_settings()
-    state_processor = StateProcessor()
-    state_processor.parameters = parameters
-    mw = MainWindow()  # noqa: F841 # pyright: ignore [reportUnusedVariable]
-    app.exec()
+    ...
--- src/qmines/utilities/__init__.py
+++
@@ -1,9 +0,0 @@
-from PySide6 import QtWidgets as QW
-from PySide6.QtGui import QAction, QFont
-
-
-def set_font_size_based_on_height(widget: QW.QWidget | QAction, height: int) -> None:
-    new_size = height // 2
-    current_font = widget.font()
-    if current_font.pointSize() != new_size:
-        widget.setFont(QFont(current_font.family(), new_size))
